46ac00dda1d84517d8f267f744b507e764a3b7cd
SKSritharan <sk.sritharan75@gmail.com>
2024-11-19 19:06:58 +0530

    Added cors middleware

---
+++ server/README.md
@@ -0,0 +1,16 @@
+# Todo App Server API Documentation
+
+## Endpoints
+
+### Create a new Todo
+
+- **URL:** `/todos`
+- **Method:** `POST`
+- **Request Body:**
+  `{
+    "text": "todo 1",
+    "completed": true
+   }
+ `
+
+
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const mongoString = process.env.DATABASE_URL;
 const routes = require("./routes/routes");
+const cors = require("cors");
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -17,8 +18,8 @@ database.once("connected", () => {
 });
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use("/api", routes);
 
 app.listen(3000, () => {
